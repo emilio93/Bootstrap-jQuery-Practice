0b8ab7ce875895a635bd99371c941b1a20b3bec4
Emilio Rojas <emilio93@gmail.com>
2021-07-28 13:14:07 -0600

    Add tests for input entry, login and logout buttons

--- tests/funcitonal.spec.ts
+++ tests/funcitonal.spec.ts
@@ -5,5 +5,13 @@ const URL = `file:///${path.dirname(__dirname)}/docs/index.html`;
 
 test('basic test', async ({ page }) => {
   await page.goto(URL);
-  await page.screenshot({ path: `screenshot.png`, fullPage: true });
+  const email = await page.$('#email');
+  await email.type("correo@gmail.com")
+  const password = await page.$('#password')
+  await password.type('miPassword');
+  await page.screenshot({ path: `screenshot-before-login.png`, fullPage: true });
+  await page.click('#login-button');
+  await page.screenshot({ path: `screenshot-after-login.png`, fullPage: true });
+  await page.click('#logout-button');
+  await page.screenshot({ path: `screenshot-after-logout.png`, fullPage: true });
 });
