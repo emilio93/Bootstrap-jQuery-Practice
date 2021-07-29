b5f0b4d191d01fce4210d4f916802910f4e079a5
Emilio Rojas <emilio.rojas@ucr.ac.cr>
2021-07-29 00:25:14 -0600

    Add functions to add and remove feedback for invalid input

--- docs/script.js
+++ docs/script.js
@@ -2,37 +2,39 @@ $( document ).ready(function() {
     const loginButton = $('#login-button');
     const logoutButton = $('#logout-button');
 
-    loginButton.on('click', function(e) {
+    const addInvalidFeedback = (formElement, message) => {
+        formElement.addClass('is-invalid');
+        if (!$(`#${formElement.attr('id')}-invalid-feedback`).length) {
+            jQuery('<div/>', {
+                "id": `${formElement.attr('id')}-invalid-feedback`,
+                "class": 'invalid-feedback',
+                "html": message
+            }).insertAfter(formElement);
+        }
+    };
+
+    const removeInvalidFeedback = (formElement) => {
+        formElement.removeClass('is-invalid');
+        $(`#${formElement.attr('id')}-invalid-feedback`).remove();
+    };
+
+    loginButton.on('click', function (e) {
         e.preventDefault();
         const email = $('#email');
         const password = $('#password');
         let stop = false;
         if (email.val().trim() === '') {
-            email.addClass('is-invalid');
-            if (!$('#email-invalid-feedback').length) {
-                jQuery('<div/>', {
-                    "id": 'email-invalid-feedback',
-                    "class": 'invalid-feedback',
-                    "html": 'Please enter an email'
-                }).insertAfter(email);
-            }
+            addInvalidFeedback(email, "Please enter an email");
             stop = true;
         } else {
-            email.removeClass('is-invalid');
+            removeInvalidFeedback(email)
         }
 
         if (password.val().trim() === '') {
-            password.addClass('is-invalid');
-            if (!$('#passowrd-invalid-feedback').length) {
-                jQuery('<div/>', {
-                    "id": 'passowrd-invalid-feedback',
-                    "class": 'invalid-feedback',
-                    "html": 'Please enter a password'
-                }).insertAfter(password);
-            }
+            addInvalidFeedback(password, "Please enter a password");
             stop = true;
         } else {
-            password.removeClass('is-invalid');
+            removeInvalidFeedback(password);
         }
         if (stop) return;
 
